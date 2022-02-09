0432d504d5610b65f81f8e3a8ac4273edb97200e
Mohammad Attari <masoud.ata@gmail.com>
2022-02-09 19:30:11 +0100

    Added a splitter to the main window for easy resizing of the timing table

--- src/window.py
+++ src/window.py
@@ -1,14 +1,15 @@
 from typing import List
 
-from PyQt5.QtWidgets import (QPushButton, QLabel, QMainWindow, QTableWidget, QTableWidgetItem,
-                             QLineEdit, QGroupBox, QFrame, QVBoxLayout, QHBoxLayout, QComboBox, QApplication, QMessageBox)
+from PyQt5.QtWidgets import (
+    QPushButton, QLabel, QMainWindow, QTableWidget, QTableWidgetItem, QLineEdit, QGroupBox,
+    QFrame, QVBoxLayout, QHBoxLayout, QComboBox, QApplication, QMessageBox, QSplitter, QWidget
+)
 from PyQt5.QtGui import QPainter
 from PyQt5.QtCore import Qt, QPoint
 
 from custom_editor import QCodeEditor
 from assembler import assemble
 from window_settings import UiSettings
-# from controller import Controller
 
 
 DEFAULT_PROGRAM = \
@@ -25,23 +26,26 @@ class MainWindow(QMainWindow):
         self.controller = controller
         self.instruction_table = {}
 
-        self.main_frame = QFrame(self)
-        self.setCentralWidget(self.main_frame)
+        self.left_frame = QFrame()
+        self.right_frame = QFrame()
+        self.left_frame.setFrameShape(QFrame.StyledPanel)
+        self.right_frame.setFrameShape(QFrame.StyledPanel)
 
         self.instruction_queue_labels: List[QLabel] = []
         self.add_sub_reservation_station_labels: List[QLabel] = []
         self.mul_div_reservation_station_labels: List[QLabel] = []
         self.load_store_reservation_station_labels: List[QLabel] = []
-        self.code_editor_status_label = QLabel(self.main_frame)
-        self.code_editor = QCodeEditor(self.main_frame)
-        self.timing_table = QTableWidget(self.main_frame)
+        self.code_editor_status_label = QLabel(self.left_frame)
+        self.code_editor = QCodeEditor(self.left_frame)
+
+        self.timing_table = QTableWidget()
 
         self.load_button = QPushButton(UiSettings.LOAD_BUTTON_TITLE)
         self.step_button = QPushButton(UiSettings.STEP_BUTTON_TITLE)
         self.run_button = QPushButton(UiSettings.RUN_BUTTON_TITLE)
 
-        self.scheduler_selector_title_label = QLabel(UiSettings.SCHEDULER_TITLE, self.main_frame)
-        self.scheduler_selector_combo_box = QComboBox(self.main_frame)
+        self.scheduler_selector_title_label = QLabel(UiSettings.SCHEDULER_TITLE, self.left_frame)
+        self.scheduler_selector_combo_box = QComboBox(self.left_frame)
 
         self.load_store_num_cycles_textbox = QLineEdit(str(self.controller.get_num_cycles_load_store()))
         self.add_sub_num_cycles_textbox = QLineEdit(str(self.controller.get_num_cycles_add_sub()))
@@ -68,8 +72,19 @@ class MainWindow(QMainWindow):
         self._init_cycles_boxes()
         self._init_rs_num_boxes()
 
+        self.splitter = QSplitter(Qt.Horizontal)
+        self.splitter.addWidget(self.left_frame)
+        self.splitter.addWidget(self.right_frame)
+        self.splitter.setSizes([200, 220])
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.splitter)
+        main_widget = QWidget()
+        main_widget.setLayout(layout)
+        self.setCentralWidget(main_widget)
         self.setGeometry(pos_x, pos_y, width, height)
         self.setWindowTitle(title)
+        self.statusBar().showMessage("")
         self.show()
 
     def _init_menu_bar(self):
@@ -105,11 +120,13 @@ class MainWindow(QMainWindow):
         self.code_editor_status_label.setStyleSheet(UiSettings.GREEN_STYLE)
 
     def _init_timing_table(self) -> None:
+        layout = QHBoxLayout()
+        layout.addWidget(self.timing_table)
+        self.right_frame.setLayout(layout)
+
         self.timing_table.setRowCount(UiSettings.NUM_ROWS_TIMING_TABLE)
         self.timing_table.setColumnCount(UiSettings.NUM_COLS_TIMING_TABLE)
         self.timing_table.setFont(UiSettings.TIMING_TABEL_FONT)
-        self.timing_table.move(UiSettings.TIMING_TABLE_POS)
-        self.timing_table.resize(UiSettings.TIMING_TABEL_SIZE)
         col_headers = [str(col_number+1) for col_number in range(UiSettings.NUM_COLS_TIMING_TABLE)]
         row_headers = [''] + ["{:<2}".format(str(row_number+1)) for row_number in range(UiSettings.NUM_ROWS_TIMING_TABLE)]
         self.timing_table.setHorizontalHeaderLabels(col_headers)
@@ -133,29 +150,29 @@ class MainWindow(QMainWindow):
 
     def _init_instruction_queue_labels(self) -> None:
         num_slots = self.controller.get_num_instruction_queue_slots()
-        instruction_queue_title_label = QLabel(UiSettings.INSTRUCTION_QUEUE_TITLE, self.main_frame)
+        instruction_queue_title_label = QLabel(UiSettings.INSTRUCTION_QUEUE_TITLE, self.left_frame)
         instruction_queue_title_label.setFont(UiSettings.SLOT_TITLE_FONT)
         instruction_queue_title_label.adjustSize()
         instruction_queue_title_label.move(UiSettings.INSTRUCTION_QUEUE_TITLE_POS - QPoint(0, num_slots * UiSettings.SLOT_HEIGHT))
         for i in range(num_slots):
-            self.instruction_queue_labels.append(QLabel("", self.main_frame))
+            self.instruction_queue_labels.append(QLabel("", self.left_frame))
             self.instruction_queue_labels[i].setFont(UiSettings.SLOT_FONT)
             self.instruction_queue_labels[i].setStyleSheet(UiSettings.WHITE_STYLE)
             self.instruction_queue_labels[i].move(UiSettings.INSTRUCTION_QUEUE_POS - QPoint(0, i * UiSettings.SLOT_HEIGHT))
             self.instruction_queue_labels[i].resize(UiSettings.SLOT_SIZE)
 
     def _init_reservation_station_title_labels(self) -> None:
-        load_store_name_label = QLabel(UiSettings.LOAD_STORE_RS_TITLE, self.main_frame)
+        load_store_name_label = QLabel(UiSettings.LOAD_STORE_RS_TITLE, self.left_frame)
         load_store_name_label.setFont(UiSettings.SLOT_TITLE_FONT)
         load_store_name_label.adjustSize()
         load_store_name_label.move(UiSettings.LOAD_STORE_RS_TITLE_POS)
 
-        add_sub_name_label = QLabel(UiSettings.ADD_SUB_RS_TITLE, self.main_frame)
+        add_sub_name_label = QLabel(UiSettings.ADD_SUB_RS_TITLE, self.left_frame)
         add_sub_name_label.setFont(UiSettings.SLOT_TITLE_FONT)
         add_sub_name_label.adjustSize()
         add_sub_name_label.move(UiSettings.ADD_SUB_RS_TITLE_POS)
 
-        mul_div_name_label = QLabel(UiSettings.MUL_DIV_RS_TITLE, self.main_frame)
+        mul_div_name_label = QLabel(UiSettings.MUL_DIV_RS_TITLE, self.left_frame)
         mul_div_name_label.setFont(UiSettings.SLOT_TITLE_FONT)
         mul_div_name_label.adjustSize()
         mul_div_name_label.move(UiSettings.MUL_DIV_RS_TITLE_POS)
@@ -182,7 +199,7 @@ class MainWindow(QMainWindow):
             label.deleteLater()
         rs_labels.clear()
         for i in range(get_num_rs()):
-            rs_labels.append(QLabel("", self.main_frame))
+            rs_labels.append(QLabel("", self.left_frame))
             rs_labels[i].show()
             rs_labels[i].setFont(UiSettings.SLOT_FONT)
             rs_labels[i].setStyleSheet(UiSettings.WHITE_STYLE)
@@ -200,7 +217,7 @@ class MainWindow(QMainWindow):
         self.run_button.setFont(UiSettings.BUTTONS_FONT)
         self.run_button.clicked.connect(self._run_button_pressed)
 
-        buttons_group = QGroupBox("", self.main_frame)
+        buttons_group = QGroupBox("", self.left_frame)
         layout = QVBoxLayout()
         layout.addWidget(self.load_button)
         layout.addWidget(self.step_button)
@@ -220,7 +237,7 @@ class MainWindow(QMainWindow):
         self.scheduler_selector_title_label.setFont(UiSettings.BUTTONS_FONT)
 
     def _init_cycles_boxes(self) -> None:
-        num_cycles_group = QGroupBox("", self.main_frame)
+        num_cycles_group = QGroupBox("", self.left_frame)
         outer_layout = QHBoxLayout()
         left_layout = QVBoxLayout()
         right_layout = QVBoxLayout()
@@ -243,7 +260,7 @@ class MainWindow(QMainWindow):
         num_cycles_group.resize(UiSettings.NUM_CYCLES_TEXTBOX_SIZE)
 
     def _init_rs_num_boxes(self) -> None:
-        rs_num_group = QGroupBox("", self.main_frame)
+        rs_num_group = QGroupBox("", self.left_frame)
         outer_layout = QHBoxLayout()
         left_layout = QVBoxLayout()
         right_layout = QVBoxLayout()
@@ -409,16 +426,16 @@ class MainWindow(QMainWindow):
         self.controller.set_reservation_station_sizes(load_store_rs_nums, add_sub_rs_nums, mul_div_rs_nums)
 
     def _draw_wires(self) -> None:
-        offset_y = self.main_frame.pos().y()
-        offset_x = self.main_frame.pos().x()
+        offset_y = self.left_frame.pos().y() + 30
+        offset_x = self.left_frame.pos().x()
         painter = QPainter(self)
         painter.setPen(Qt.black)
         painter.setBrush(Qt.white)
         painter.drawLine(400+offset_x, 330+offset_y, 400+offset_x, 380+offset_y)
-        painter.drawLine(120+offset_x, 380+offset_y, 600+offset_x, 380+offset_y)
-        painter.drawLine(120+offset_x, 380+offset_y, 120+offset_x, 420+offset_y)
+        painter.drawLine(140+offset_x, 380+offset_y, 620+offset_x, 380+offset_y)
+        painter.drawLine(140+offset_x, 380+offset_y, 140+offset_x, 420+offset_y)
         painter.drawLine(400+offset_x, 380+offset_y, 400+offset_x, 420+offset_y)
-        painter.drawLine(600+offset_x, 380+offset_y, 600+offset_x, 420+offset_y)
+        painter.drawLine(620+offset_x, 380+offset_y, 620+offset_x, 420+offset_y)
 
     def paintEvent(self, event) -> None:
         self._draw_wires()
